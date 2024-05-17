6dda15ca53f274d813d3e2e30e41e147c312965b
Maximo Agoglia <magoglia@fb.com>
2024-05-16 23:00:49 -0500

    Initial Commit

--- functions.py
+++ functions.py
@@ -1,19 +1,23 @@
-FILEPATH = "/Users/magoglia/Desktop/web_app1/pythonProject1/todos.txt"
+FILEPATH = "todos.txt"
+
 
 def get_todos(filepath=FILEPATH):
-    """Read a text file and return the lift of
+    """ Read a text file and return the list of
     to-do items.
     """
     with open(filepath, 'r') as file_local:
         todos_local = file_local.readlines()
     return todos_local
+
+
 def write_todos(todos_arg, filepath=FILEPATH):
-    """Write the to-do items list in the text file."""
-    with open(filepath, 'w') as file_local:
-        file_local.writelines(todos_arg)
+    """ Write the to-do items list in the text file."""
+    with open(filepath, 'w') as file:
+        file.writelines(todos_arg)
 
 
-if __name__ == '__main__':
-    print('Hello')
+if __name__ == "__main__":
+    print("Hello")
+    print(get_todos())
 
 
