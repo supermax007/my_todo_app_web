2ba8aa86c56f8f864ecba4b119a55affd48e1652
Maximo Agoglia <magoglia@fb.com>
2024-05-19 18:43:23 -0500

    Initial Commit

--- cameraman.py
+++ cameraman.py
@@ -1,11 +1,18 @@
 import streamlit as st
 from PIL import Image
 
+
+uploaded_image = st.file_uploader("Upload Image", type=["jpg", "png", "jpeg"])
+
+if uploaded_image:
+    img = Image.open(uploaded_image)
+    gray_uploaded_img = img.convert('L')
+    st.image(gray_uploaded_img)
+
 with st.expander('Start Camera'):
     camera_image = st.camera_input("Camera")
 
 #Start the Camera
-
     if camera_image:
 # Create a pillow image instance
         img = Image.open(camera_image)
@@ -15,4 +22,5 @@ with st.expander('Start Camera'):
 #Render grayscale image
         st.image(gray_img)
         st.header("BAWWWWWS")
+
 
